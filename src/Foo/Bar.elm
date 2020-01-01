91e94a514648077e54d04cfbe3315ff80aa45499
choonkeat <choonkeat@gmail.com>
2020-01-01 23:35:05 +0800

    avoid module comments overwriting previous module comments
    otherwise elm-verify-examples will only generate test using the last one

--- src/Foo/Bar.elm
+++ src/Foo/Bar.elm
@@ -12,9 +12,35 @@ module Foo.Bar exposing (..)
 
     encodeFooBarOption Json.Encode.int optionValue
         |> Json.Encode.encode 0
-        |> Debug.log "encoded Option"
         |> Json.Decode.decodeString (decodeFooBarOption Json.Decode.int)
     --> Ok optionValue
+
+    helloValue : Hello Int
+    helloValue =
+        Good "Morning" "5.45"
+
+    encodeFooBarHello Json.Encode.int helloValue
+        |> Json.Encode.encode 0
+        |> Json.Decode.decodeString (decodeFooBarHello Json.Decode.int)
+    --> Ok helloValue
+
+    personValue : Person
+    personValue =
+        { name = "Foo", age = 42 }
+
+    encodeFooBarPerson personValue
+        |> Json.Encode.encode 0
+        |> Json.Decode.decodeString decodeFooBarPerson
+    --> Ok personValue
+
+    payloadValue : Payload
+    payloadValue =
+        { title = "Hello", author = { name = "Foo", age = 42 } }
+
+    encodeFooBarPayload payloadValue
+        |> Json.Encode.encode 0
+        |> Json.Decode.decodeString decodeFooBarPayload
+    --> Ok payloadValue
 
 -}
 
@@ -30,91 +56,22 @@ type alias Choice =
     Option Bool
 
 
-{-|
-
-    import Json.Encode
-    import Json.Decode
-    import Foo.Bar.Auto exposing (..)
-
-    helloValue : Hello Int
-    helloValue =
-        Good "Morning" 5.45
-
-    encodeFooBarHello Json.Encode.int helloValue
-        |> Json.Encode.encode 0
-        |> Debug.log "encoded Hello"
-        |> Json.Decode.decodeString (decodeFooBarHello Json.Decode.int)
-    --> Ok helloValue
-
--}
 type Hello x
     = Hello
     | Good String String
 
 
-{-|
-
-    import Json.Encode
-    import Json.Decode
-    import Foo.Bar.Auto exposing (..)
-
-    personValue : Person
-    personValue =
-        { name = "Foo", age = 42 }
-
-    encodeFooBarPerson personValue
-        |> Json.Encode.encode 0
-        |> Debug.log "encoded Person"
-        |> Json.Decode.decodeString decodeFooBarPerson
-    --> Ok personValue
-
--}
 type alias Person =
     { name : String
     , age : Int
     }
 
 
-{-|
-
-    import Json.Encode
-    import Json.Decode
-    import Foo.Bar.Auto exposing (..)
-
-    payloadValue : Payload
-    payloadValue =
-        { title = "Hello", author = { name = "Foo", age = 42 } }
-
-    encodeFooBarPayload payloadValue
-        |> Json.Encode.encode 0
-        |> Debug.log "encoded Payload"
-        |> Json.Decode.decodeString decodeFooBarPayload
-    --> Ok payloadValue
-
--}
 type alias Payload =
     { title : String
     , author : Person
     }
 
 
-{-|
-
-    import Dict
-    import Json.Encode
-    import Json.Decode
-    import Foo.Bar.Auto exposing (..)
-
-    lookupValue : Lookup
-    lookupValue =
-        Dict.fromList [ ("one", 1), ("two", 2) ]
-
-    encodeFooBarLookup lookupValue
-        |> Json.Encode.encode 0
-        |> Debug.log "encoded Lookup"
-        |> Json.Decode.decodeString decodeFooBarLookup
-    --> Ok lookupValue
-
--}
 type alias Lookup =
     Dict String Int
