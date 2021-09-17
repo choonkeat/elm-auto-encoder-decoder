1c7f4b08b1b5d76a46bd1c445ba3fe8c1c6ae81b
choonkeat <choonkeat@hobnob.ai>
2021-09-17 15:18:26 +0800

    add missing default support for Result

--- src/Foo/Baz/Auto.elm
+++ src/Foo/Baz/Auto.elm
@@ -47,6 +47,16 @@ encodeArrayArray =
     Json.Encode.array
 
 
+encodeResultResult : (x -> Json.Encode.Value) -> (a -> Json.Encode.Value) -> Result.Result x a -> Json.Encode.Value
+encodeResultResult encodex encodea result =
+    case result of
+        Err x ->
+            Json.Encode.list identity [ Json.Encode.string "Err", encodex x ]
+
+        Ok a ->
+            Json.Encode.list identity [ Json.Encode.string "Ok", encodea a ]
+
+
 encodeSetSet : (comparable -> Json.Encode.Value) -> Set.Set comparable -> Json.Encode.Value
 encodeSetSet encoder =
     Set.toList >> encodeList encoder
@@ -114,6 +124,23 @@ decodeList =
 decodeArrayArray : (Json.Decode.Decoder a) -> Json.Decode.Decoder (Array.Array a)
 decodeArrayArray =
     Json.Decode.array
+
+
+decodeResultResult : Json.Decode.Decoder x -> Json.Decode.Decoder a -> Json.Decode.Decoder (Result.Result x a)
+decodeResultResult decodex decodea =
+    Json.Decode.index 0 Json.Decode.string
+        |> Json.Decode.andThen
+            (\s ->
+                case s of
+                    "Err" ->
+                        Json.Decode.map Err (Json.Decode.index 1 decodex)
+
+                    "Ok" ->
+                        Json.Decode.map Ok (Json.Decode.index 1 decodea)
+
+                    _ ->
+                        Json.Decode.fail ("Unexpected: " ++ s)
+            )
 
 
 decodeSetSet : (Json.Decode.Decoder comparable) -> Json.Decode.Decoder (Set.Set comparable)
